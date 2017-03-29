c5ed8235a5d377cab0b3d38c43be344d44f03e56
Adam-Davenport <adavenpo@purdue.edu>
2017-03-28 23:46:14 -0400

    Setting up encounters app

--- dmtools/settings.py
+++ dmtools/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'statics')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'enounters',
+	'encounters',
 	'loot'
 ]
 
--- encounters/views.py
+++ encounters/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def encounters(request):
+	return render(request, 'encounters/encounters.html')

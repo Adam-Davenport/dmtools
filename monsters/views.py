15209772497f20bc585c50d138ae072aa2d15a39
adam <adavenpo@purdue.edu>
2017-03-30 14:55:07 -0400

    Changing show function to monsters to reference the fact that it is the root monsters page

--- monsters/views.py
+++ monsters/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def new(request):
     return(render('monsters/new.html'))
-def show(request):
+def monsters(request):
     return(render('monsters/show.html'))

88ba8ed1f47a2d77b9f0dcd4b189316d321c7753
adam <adavenpo@purdue.edu>
2017-03-30 15:06:31 -0400

    Inserting missing comma

--- dmtools/urls.py
+++ dmtools/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-	url(r'^encounters/', include('encounters.urls'))
+	url(r'^encounters/', include('encounters.urls')),
     url(r'^monsters/', include('monsters.urls')),
     url(r'^loot/', include('loot.urls'))
 ]

2c216251254966691515eac7cbce6538161c09d6
Adam-Davenport <adavenpo@purdue.edu>
2017-03-30 22:26:15 -0400

    Adding in function to handle input in xdy format

--- resources/dice.py
+++ resources/dice.py
@@ -7,3 +7,12 @@ def roll_dice(number, dice):
     results = []
     for i in range(number):
         results.append(roll_die(dice))
+
+def check_dice(input):
+	if 'd' in input:
+		input = input.split('d')
+		d = int(input[0])
+		n = int(input[1])
+		return roll_dice(d,n)
+	else:
+		return roll_die(input)

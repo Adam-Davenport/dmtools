238124bc7a4cdd831aefd8ccb6def72ae190bb05
Adam-Davenport <adavenpo@purdue.edu>
2017-03-30 20:40:50 -0400

    Creating monster model

--- monsters/models.py
+++ monsters/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Monster(models.Model):
+	name = models.CharField(max_length=255)
+	# Setting up choices for environments
+	Aquatic = 'Aquatic'
+	Desert = 'Desert'
+	Forest = 'Forest'
+	Hill = 'Hill'
+	Marsh = 'Marsh'
+	Plain = 'Plain'
+	Underground = 'Underground'
+	terrain_choices = (
+		(Aquatic, 'Aquatic'),
+		(Desert, 'Desert'),
+		(Forest, 'Forest'),
+		(Hill, 'Hill'),
+		(Marsh, 'Marsh'),
+		(Plain, 'Plain'),
+		(Underground, 'Underground')
+	)
+	terrain = models.CharField(
+		max_length = 128,
+		choices = terrain_choices,
+		default = Aquatic
+	)

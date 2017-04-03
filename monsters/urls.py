0d0676e65016d73df763026801a31640de3906d8
Adam-Davenport <adavenpo@purdue.edu>
2017-04-02 20:43:18 -0400

    Adding names to views

--- monsters/urls.py
+++ monsters/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.monsters),
-    url(r'^new', views.new)
+	url(r'^$', views.monsters, name='monsters'),
+    url(r'^new', views.new, name='new')
 ]

f01ec37a12c5222d05ccb2717dffdc6ab7bc9973
Adam-Davenport <adavenpo@purdue.edu>
2017-04-16 22:01:11 -0400

    Registering models

--- monsters/admin.py
+++ monsters/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from monsters.models import Monster_Environment, Monster
 
 # Register your models here.
+admin.site.register(Monster)
+admin.site.register(Monster_Environment)

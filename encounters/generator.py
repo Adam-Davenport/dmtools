9abc435b1d8a17f4b9811dcdcb9f5b32ebbf57df
Adam-Davenport <adavenpo@purdue.edu>
2017-04-16 22:00:21 -0400

    Creating file for encounter generation

---
+++ encounters/generator.py
@@ -0,0 +1,13 @@
+from monsters.models import Monster_Environment
+import random
+
+def random_encounter(environment):
+	#Assuming evnironment is already checked
+	try:
+		monsters = Monster_Environment.objects.all()
+		for m in monsters:
+			print(m.name)
+	except:
+		print('nope')
+
+

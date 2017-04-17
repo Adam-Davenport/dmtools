e1f3cd88e2344fc3dd5c512e7f63207083b240c1
Adam-Davenport <adavenpo@purdue.edu>
2017-04-16 21:58:38 -0400

    Testing random encounter

--- encounters/views.py
+++ encounters/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from encounters.generator import random_encounter
 
 # View functions
 def encounters(request):
+	random_encounter('Aquatic')
 	if request.method == 'POST':
 		return render(request, 'encounters/encounters.html')
 	else:

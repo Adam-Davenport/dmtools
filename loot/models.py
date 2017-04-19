240adf1b6c4695ce3a9dde4d6ad3c2bc3ab34183
adam <adavenpo@purdue.edu>
2017-04-19 12:21:04 -0400

    Creating loot model

--- loot/models.py
+++ loot/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from math import floor
 
-# Create your models here.
+
+class Loot(models.Model):
+    name = models.CharField(max_length=128)
+    cost = models.IntegerField()
+
+    def expanded_cost(self):
+        gold = floor(self.cost)
+        remainder = (self.cost % 1) * 10
+        silver = floor(remainder)
+        remainder = (remainder % 1) * 10
+        copper = floor(remainder)
+        return [gold, silver, copper]

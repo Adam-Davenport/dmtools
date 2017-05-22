28d470a5d002b2a1e662872b127413187a54623e
Adam-Davenport <adavenpo@purdue.edu>
2017-05-22 00:31:12 -0400

    Importing django generic class views

--- monsters/views.py
+++ monsters/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import(
+    ListView, CreateView, DetailView, UpdateView, DeleteView, TemplateView
+)
 
 # Create your views here.
 def new(request):

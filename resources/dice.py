1b7eb7736f424f1bbdedf3c90b03dbd4f57a2386
Adam-Davenport <adavenpo@purdue.edu>
2017-05-22 01:01:01 -0400

    Pep8 formating

--- resources/dice.py
+++ resources/dice.py
@@ -1,18 +1,21 @@
 from random import randint
 
+
 def roll_die(dice):
     return (randint(1, dice))
+
 
 def roll_dice(number, dice):
     results = []
     for i in range(number):
         results.append(roll_die(dice))
 
+
 def check_dice(input):
     if 'd' in input:
         input = input.split('d')
         d = int(input[0])
         n = int(input[1])
-        return roll_dice(d,n)
+        return roll_dice(d, n)
     else:
         return roll_die(input)

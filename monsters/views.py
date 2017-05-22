f3c9815e89e64670ba125dbc977037d4187f528f
Adam-Davenport <adavenpo@purdue.edu>
2017-05-22 00:41:40 -0400

    Adding form class for create view

--- monsters/views.py
+++ monsters/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from monster.models import Monster
+from monsters.forms import CreateMonsterForm
 from django.views.generic import (
     ListView, CreateView, DetailView, UpdateView, DeleteView, TemplateView
 )
@@ -8,6 +9,7 @@ from django.views.generic import (
 class Monsters(ListView):
     model = Monster
     template_name = 'monsters/index.html'
+    form_class = CreateMonsterForm
 
     def get_queryset(self):
         return Monster.objects.all().order_by('name')

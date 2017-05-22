414894fec696fb47192cafc14d059bc63c4c7c1d
Adam-Davenport <adavenpo@purdue.edu>
2017-05-22 00:29:24 -0400

    Converting tabs to spaces

--- encounters/views.py
+++ encounters/views.py
@@ -3,8 +3,7 @@ from encounters.generator import random_encounter
 
 # View functions
 def encounters(request):
-	random_encounter('Aquatic')
-	if request.method == 'POST':
-		return render(request, 'encounters/encounters.html')
-	else:
-		return render(request, 'encounters/encounters.html')
+    if request.method == 'POST':
+        return render(request, 'encounters/encounters.html')
+    else:
+        return render(request, 'encounters/encounters.html')

c4a5a4dc307f5fc9f933be5ef9aef4238c0bdb7b
Adam-Davenport <adavenpo@purdue.edu>
2017-05-22 22:00:38 -0400

    Fleshing out the monster model more and separating attacks to their own model

--- monsters/models.py
+++ monsters/models.py
@@ -3,24 +3,48 @@ from django.db import models
 
 class Monster(models.Model):
     name = models.CharField(max_length=255)
-    levels = models.IntegerField()
-    hit_die = models.IntegerField()
-    challenge_rating = models.IntegerField()
+    levels = models.IntegerField(default=1)
+    hit_die = models.IntegerField(default=1)
+    # Monster size
+    size_choices = (
+        ('T', 'Tiny')
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('H', 'Huge'),
+        ('G', 'Gargantuan')
+    )
+    size = models.CharField(choices=size_choices, default='M', max_length=1)
+    challenge_rating = models.IntegerField(default=1)
     # Ability Scores
-    strength = models.IntegerField()
-    dexterity = models.IntegerField()
-    constitution = models.IntegerField()
-    intelligence = models.IntegerField()
-    wisdom = models.IntegerField()
-    charisma = models.IntegerField()
-    special_abilities = models.TextField()
+    strength = models.IntegerField(default=10)
+    dexterity = models.IntegerField(default=10)
+    constitution = models.IntegerField(default=10)
+    intelligence = models.IntegerField(default=10)
+    wisdom = models.IntegerField(default=10)
+    charisma = models.IntegerField(default=10)
 
     def __str__(self):
         return self.name
 
 
+class Monster_Attacks(models.Model):
+    monster = models.ForeignKey(Monster)
+    # Ability score choices
+    ability_choices = (
+        ('str', 'Strength'),
+        ('dex', 'Dexterity'),
+        ('con', 'Constitution'),
+        ('int', 'Intelligence'),
+        ('wis', 'Wisdom'),
+        ('cha', 'Charisma')
+    )
+    ability_modifier = models.CharField()
+    ability_damage = models.BooleanField()
+
+
 class Monster_Environment(models.Model):
-    name = models.ForeignKey(Monster)
+    monster = models.ForeignKey(Monster)
     # Setting up choices for environments
     Aquatic = 'Aquatic'
     Desert = 'Desert'

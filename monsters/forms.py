6aa5ff731250a2ec31ff7bfb8f3e8531c2196859
Adam-Davenport <adavenpo@purdue.edu>
2017-06-02 11:06:29 -0400

    Moving meta fields into class meta

--- monsters/forms.py
+++ monsters/forms.py
@@ -3,15 +3,18 @@ from monsters.models import Monster, Monster_Attacks, Monster_Environment
 
 
 class MonsterForm(forms.Form):
-    model = Monster
-    fields = '__all__'
+    class Meta:
+        model = Monster
+        fields = '__all__'
 
 
 class MonsterAttackForm(forms.Form):
-    model = Monster_Attacks
-    fields = '__all__'
+    class Meta:
+        model = Monster_Attacks
+        fields = '__all__'
 
 
 class MonsterEnvironmentForm(forms.Form):
-    model = Monster_Environment
-    fields = '__all__'
+    class Meta:
+        model = Monster_Environment
+        fields = '__all__'

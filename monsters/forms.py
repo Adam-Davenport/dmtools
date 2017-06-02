b89b75ecbd7190ec9a88ff258a32054f66a2dd6f
Adam-Davenport <adavenpo@purdue.edu>
2017-06-02 10:58:16 -0400

    Creating forms for all monster models

--- monsters/forms.py
+++ monsters/forms.py
@@ -1,7 +1,17 @@
 from django import forms
-from monsters.models import Monster
+from monsters.models import Monster, Monster_Attacks, Monster_Environment
 
 
 class CreateMonsterForm(forms.Form):
     model = Monster
+    fields = '__all__'
+
+
+class MonsterAttackForm(forms.Form):
+    model = Monster_Attacks
+    fields = '__all__'
+
+
+class MonsterEnvironmentForm(forms.form):
+    model = Monster_Environment
     fields = '__all__'

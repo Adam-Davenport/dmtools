539c67f03f7cb115398cdf4223a0a5dc67d424b6
Adam-Davenport <adavenpo@purdue.edu>
2017-06-07 21:25:32 -0400

    Adding details, update, and delete urls

--- monsters/urls.py
+++ monsters/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Monsters.as_view(), name='index'),
-    url(r'^new$', views.MonsterCreate.as_view(), name='new')
+    url(r'^new$', views.MonsterCreate.as_view(), name='new'),
+    url(r'^/(?P<pk>\d)$', views.MonsterDetails.as_view(), name='details'),
+    url(r'^/(?P<pk>\d)/update$', views.MonsterUpdate.as_view(), name='update'),
+    url(r'^/(?P<pk>\d)/delete$', views.MonsterDelete.as_view(), name='delete'),
 ]

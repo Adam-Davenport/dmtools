2d003cc105550b18b628a1ec9340921a6cacc60b
Adam-Davenport <adavenpo@purdue.edu>
2017-06-08 22:14:32 -0400

    Changing size to select

--- monsters/forms.py
+++ monsters/forms.py
@@ -10,7 +10,7 @@ class MonsterForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'levels': forms.NumberInput(attrs={'class': 'form-control'}),
             'hit_die': forms.NumberInput(attrs={'class': 'form-control'}),
-            'size': forms.TextInput(attrs={'class': 'form-control'}),
+            'size': forms.Select(attrs={'class': 'form-control'}),
             'challenge_rating': forms.NumberInput(attrs={'class': 'form-control'}),
             'strength': forms.NumberInput(attrs={'class': 'form-control'}),
             'dexterity': forms.NumberInput(attrs={'class': 'form-control'}),

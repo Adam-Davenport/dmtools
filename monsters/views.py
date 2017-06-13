b5368580327b870a8501a26285ce794e3ff34a48
Adam-Davenport <adavenpo@purdue.edu>
2017-06-13 11:57:38 -0400

    Overriding get_object of monsterdetails so that data can be calculated for hte template

--- monsters/views.py
+++ monsters/views.py
@@ -25,6 +25,16 @@ class MonsterDetails(DetailView):
     model = Monster
     template_name = 'monsters/details.html'
 
+    def get_object(self, queryset=None):
+        monster = super(MonsterDetails, self).get_object(queryset=queryset)
+        monster.health = '{}d{}+{} ({})'.format(
+            monster.level,
+            monster.hit_die,
+            str(monster.modifiers()[2]*monster.level),
+            str(monster.level*((int(monster.hit_die/2))+1))
+        )
+        return monster
+
 
 class MonsterUpdate(LoginRequiredMixin, UpdateView):
     model = Monster

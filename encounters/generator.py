5f2ea777d8736bed7bfeb3b794d87c66cb301ad5
Adam-Davenport <adavenpo@purdue.edu>
2017-06-13 20:43:18 -0400

    Formatting

--- encounters/generator.py
+++ encounters/generator.py
@@ -1,13 +1,12 @@
 from monsters.models import Monster_Environment
 import random
 
+
 def random_encounter(environment):
-	#Assuming evnironment is already checked
-	try:
-		monsters = Monster_Environment.objects.all()
-		for m in monsters:
-			print(m.name)
-	except:
-		print('nope')
-
-
+    # Assuming evnironment is already checked
+    try:
+        monsters = Monster_Environment.objects.all()
+        for m in monsters:
+            print(m.name)
+    except:
+        print('nope')

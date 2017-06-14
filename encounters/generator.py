1ff20753b7bf664d87f580f546f8633998f702b3
Adam-Davenport <adavenpo@purdue.edu>
2017-06-13 20:45:44 -0400

    Filtering by environment

--- encounters/generator.py
+++ encounters/generator.py
@@ -5,7 +5,7 @@ import random
 def random_encounter(environment):
     # Assuming evnironment is already checked
     try:
-        monsters = Monster_Environment.objects.all()
+        monsters = Monster_Environment.objects.filter(environment=environment)
         for m in monsters:
             print(m.name)
     except:

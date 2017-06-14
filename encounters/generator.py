05e87ff35aed942dc19e4887b5f58b718f28a210
Adam-Davenport <adavenpo@purdue.edu>
2017-06-14 01:24:28 -0400

    Format

--- encounters/generator.py
+++ encounters/generator.py
@@ -6,6 +6,6 @@ class Random_Encounter():
 
     def __init__(self, environment):
         self.environment = environment
-    
+
     def get_monsters(self):
         monsters = Monster.objects.filter(environment=self.environment)

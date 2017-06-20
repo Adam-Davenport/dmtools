19cbc3463e0ea9617492d23747e516f6e4c68ae6
Adam-Davenport <adavenpo@purdue.edu>
2017-06-20 14:31:51 -0400

    Adding cr and difficulty

--- encounters/generator.py
+++ encounters/generator.py
@@ -4,8 +4,10 @@ import random
 
 class Random_Encounter():
 
-    def __init__(self, environment):
+    def __init__(self, environment, cr, difficulty):
         self.environment = environment
+        self.cr = cr
+        self.difficulty = difficulty
 
     def get_monsters(self):
         monsters = Monster.objects.filter(environment=self.environment)
